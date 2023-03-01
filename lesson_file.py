33d3600a7efef0f8f60ef6d0963b64e89f13f935
ievastei <125908513+ievastei@users.noreply.github.com>
2023-03-01 22:20:10 +0200

    Update lesson_file.py

--- lesson_file.py
+++ lesson_file.py
@@ -1,6 +1,26 @@
+1 task
 a=10
 b=20
 if a==b:
     print ("equal")
 else:
     print ("not equal")
+
+2 task
+name = input("please enter your name ")
+age = int(input("please enter your age "))
+if age == 18:
+    print("you are welcome")
+elif age >= 18:
+    print ("you are welcome")
+else:
+    print("next time,baby")
+    
+ 3 task
+for a in range (0, 100):
+    if a % 7 == 0:
+        print(a)
+ 4 task
+word = input("please write a word, you want to reverse ")
+reverse = word [::-1]
+print(reverse)
