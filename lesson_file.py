22a339671ab808e8168bc2c76ffdd0d64520ce57
ievastei <125908513+ievastei@users.noreply.github.com>
2023-03-06 15:48:10 +0200

    Update lesson_file.py

--- lesson_file.py
+++ lesson_file.py
@@ -24,3 +24,37 @@ for a in range (0, 100):
 word = input("please write a word, you want to reverse ")
 reverse = word [::-1]
 print(reverse)
+
+5 task ( current date - deltatime)
+from datetime import date, timedelta
+dt = date.today() - timedelta(4)
+print('Current Date :',date.today())
+print('4 days before Current date is:',dt)
+
+6 task reverse a string
+def my_function(x):
+    return x[::-1]
+
+mytxt = my_function("1234abcd")
+
+print(mytxt)
+7 task find the max of three numbers
+
+def maximum(a, b, c):
+
+    if (a >= b) and (a >= c):
+        largest = a
+
+    elif (b >= a) and (b >= c):
+        largest = b
+    else:
+        largest = c
+
+    return largest
+
+
+# Driven code
+a = 10
+b = 14
+c = 12
+print(maximum(a, b, c))
